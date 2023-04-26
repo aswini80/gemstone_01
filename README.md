df8174e285aace0e9cd35549e1898404d46e7284
iamdevops <aswinikk@gmail.com>
2023-04-26 19:55:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## End to End ML project
+
+## Created a environment
+
+```
+conda create -p venv python=3.8
+```
+## Install all the necessary libraries
+
+```
+pip install -r requiremnets.txt
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path:str)->List[str]:
+    requirement=[]
+    with open(file_path) as file_obj:
+        requirement=file_obj.readlines()
+        requirement=[req.replace('\n','') for req in requirement]
+
+        if HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+
+        return requirement
+
+
+
+setup(
+name="RegressionProject",
+version="0.0.1",
+author="Aswini",
+author_email="aswinikk@gmail.com",
+install_requires=get_requirements('requirements.txt'),
+packages=find_packages()
+)
